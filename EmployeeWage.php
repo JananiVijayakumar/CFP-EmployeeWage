125f8bee84b73bd2e58eee649315d28ff5bf47fb
Janani <vsvprema@gmail.com>
2021-12-23 10:41:54 +0530

    Added: Check Employee is present or absent

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,2 +1,17 @@
 <?php
-echo "Welcome to Employee Wage Program";
+class EmployeeWage
+{
+    function getEmpAttendance()
+    {
+        $IS_PRESENT = 1;
+        $empCheck = rand(0, 1);
+        echo "Employee Attendance : $empCheck";
+        if ($empCheck == $IS_PRESENT) {
+            echo "\nEmployee is Present";
+        } else {
+            echo "\nEmployee is Absent";
+        }
+    }
+}
+$obj = new EmployeeWage();
+$obj->getEmpAttendance();
