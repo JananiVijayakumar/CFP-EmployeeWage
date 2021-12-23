c4ec71599df99945631a51d58a85be28e7d989ce
Janani <vsvprema@gmail.com>
2021-12-23 11:32:06 +0530

    Added: Check Employee Monthly Wage

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,32 +1,37 @@
 <?php
 class EmployeeWage
 {
-    function checkDailyWage()
+    function calculateEmployeeWage()
     {
         $IS_PRESENT = 1;
-        $IS_FULL_TIMER = 2;
-        $IS_PART_TIMER = 3;
+        $IS_FULL_TIMER = 1;
+        $IS_PART_TIMER = 2;
         $wagePerHour = 20;
         $fullTimeEmpWorkingHour = 8;
         $partTimeEmpWorkingHour = 8;
+        $workingDaysPerMonth = 20;
         $empCheck = rand(0, 1);
         echo "Employee Attendance : $empCheck";
         $fullTimeEmpWagePerDay = $wagePerHour * $fullTimeEmpWorkingHour;
         $partTimeEmpWagePerDay = $wagePerHour * $partTimeEmpWorkingHour;
+        $totalWagePerMonth = $workingDaysPerMonth * $fullTimeEmpWagePerDay;
         switch ($empCheck == $IS_PRESENT) {
             case $IS_FULL_TIMER:
-                echo "\nEmployee is Present";
-                echo "\nFull time Employee Daily Wage is : $fullTimeEmpWagePerDay";
+                echo "\n Employee is Present";
+                echo "\n Full time Employee Daily Wage is : $fullTimeEmpWagePerDay";
+                echo "\n Full time Employee Monthly Wage is : $totalWagePerMonth";
                 break;
             case $IS_PART_TIMER:
-                echo "\nEmployee is Present";
-                echo "\nPart time Employee Daily Wage is : $partTimeEmpWagePerDay";
+                echo "\n Employee is Present";
+                echo "\n Part time Employee Daily Wage is : $partTimeEmpWagePerDay";
+                echo "\n Part time Employee Monthly Wage is : $totalWagePerMonth";
                 break;
             default:
-                echo "\nEmployee is Absent";
+                echo "\n Employee is Absent";
+                echo "\n Employee Monthly Wage is : 0";
                 break;
         }
     }
 }
 $obj = new EmployeeWage();
-$obj->checkDailyWage();
+$obj->calculateEmployeeWage();
