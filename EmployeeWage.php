6995bb02cb70c8f6771813c69f088f7ef7683c5b
Janani <vsvprema@gmail.com>
2021-12-25 11:47:16 +0530

    Added: Add Total wage for company

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -9,46 +9,41 @@ class EmployeeWage
 {
     public $IS_FULL_TIMER = 1;
     public $IS_PART_TIMER = 2;
-    public $WORKING_DAYS_PER_MONTH = 20;
-    public $WORKING_HOUR_PER_MONTH = 100;
-    public $wagePerHour = 20;
     public $totalEmpWorkingHour = 0;
     public $EmpWorkingHour = 0;
-    public $totalEmpWorkingDays = 0;
-    public $fullTimeEmpWorkingHour = 8;
-    public $partTimeEmpWorkingHour = 8;
-    function calculateEmployeeWage($companyName, $workingHours, $WorkingDays)
+    public $WAGE_PER_DAY = 0;
+    public $TOTAL_WAGE = 0;
+    function calculateEmployeeWage($companyName, $wagePerHour, $workingDays)
     {
-        for ($i = 0; $i < $this->WORKING_DAYS_PER_MONTH; $i++) {
-            while ($this->totalEmpWorkingHour <= $this->WORKING_HOUR_PER_MONTH || $this->totalEmpWorkingDays < $this->WORKING_DAYS_PER_MONTH) {
-                $this->totalEmpWorkingDays++;
-                $empCheck = rand(0, 1);
-                echo "\nEmployee Attendance : $empCheck";
-                switch ($empCheck) {
-                    case $this->IS_FULL_TIMER:
-                        echo "\n Employee is Present";
-                        $empHour = 8;
-                        break;
-                    case $this->IS_PART_TIMER:
-                        echo "\n Employee is Present";
-                        $empHour = 4;
-                        break;
-                    default:
-                        echo "\n Employee is Absent";
-                        $empHour = 0;
-                        echo "\n Employee Monthly Wage is : 0";
-                        break;
-                }
-                $this->totalEmpWorkingHour += $empHour;
-                echo "\nEmployee Working Day : " . $this->totalEmpWorkingDays;
-                echo "\nEmployee Hours :" . $empHour;
-                $totalEmpwage = $this->totalEmpWorkingHour * $this->wagePerHour;
-                echo "\nTotal Employee Wage :" . $totalEmpwage;
-                $multipleCompanyDetails = array("Comapany Name :" . $companyName, "Total Working Hours :" . $workingHours, "Total Working Days :" . $i, "Total Wage :" . $totalEmpwage);
-                print_r($multipleCompanyDetails);
+        for ($i = 1; $i < $workingDays; $i++) {
+            $empCheck = rand(0, 1);
+            echo "\nEmployee Attendance : $empCheck";
+            switch ($empCheck) {
+                case $this->IS_FULL_TIMER:
+                    echo "\n Employee is Present";
+                    $empHour = 8;
+                    break;
+                case $this->IS_PART_TIMER:
+                    echo "\n Employee is Present";
+                    $empHour = 4;
+                    break;
+                default:
+                    echo "\n Employee is Absent";
+                    $empHour = 0;
+                    echo "\n Employee Monthly Wage is : 0";
+                    break;
             }
+            if ($this->totalWorkingHour >= 100 || $i == 20) {
+                break;
+            }
+            $this->WAGE_PER_DAY = $wagePerHour * $empHour;
+            $this->totalEmpwage += $this->WAGE_PER_DAY;
+            $this->totalWorkingHour += $empHour;
         }
+        $multipleCompanyDetails = array("Comapany Name :" . $companyName, "Total Working Hours :" . $this->totalWorkingHour, "Total Working Days :" . $i, "Total Wage :" . $this->totalEmpwage);
+        print_r($multipleCompanyDetails);
     }
 }
+
 $empWage = new EmployeeWage();
 $empWage->calculateEmployeeWage("TATA", "8", "22");
